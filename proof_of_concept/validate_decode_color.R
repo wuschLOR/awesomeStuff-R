b931543ee2e5a2acfaaf08578d98f6de65a74613
Michael Groh <m.groh@elphenbeinturm.de>
2015-12-01 18:07:30 +0100

    Update validate_decode_color.R
    spelling

--- proof_of_concept/validate_decode_color.R
+++ proof_of_concept/validate_decode_color.R
@@ -11,12 +11,12 @@ library("ggplot2")
 # validation
 decode_and_validate<- function(vector_of_values, vector_of_possible_values, vector_of_possible_lables ){
   
-  # save the origional data
+  # save the original data
   output_df <- data.frame(values_original= vector_of_values)
   
   # convert it to a factor with the right order as the vector_of_possible_lables
-  # indicates. With this transformation all values not speciefied in the 
-  # vector_of_possible_values are truned NA
+  # indicates. With this transformation all values not specified in the 
+  # vector_of_possible_values are turned NA
   output_df$values_factor   <- factor(x = output_df$values_original
                                       , levels = vector_of_possible_values
                                       , labels = vector_of_possible_lables)
@@ -24,7 +24,7 @@ decode_and_validate<- function(vector_of_values, vector_of_possible_values, vect
   
   # convert to character
   output_df$values_for_math <- as.character(x = output_df$values_factor)
-  # convert to numeric - everything that is not a number is turned to na so that
+  # convert to numeric - everything that is not a number is turned to NA so that
   # there is only a clean vector for doing math things
   suppressWarnings(
     output_df$values_for_math <- as.numeric(x = output_df$values_for_math)
@@ -39,13 +39,13 @@ create_color_pallet <- function(vector_of_possible_values){
   # herefore he needs to know how many different happy bars should be there
   how_many_numbers = length(vector_of_possible_values)
   
-  # one of the little bars doesnt allways be there so he checkts if the grumpy 
-  # missing value wants to be in this picture
+  # one of the little bars doesn't want to be there all the time he checks if 
+  # the grumpy missing value wants to be in this picture
   if (99 %in% vector_of_possible_values)
     how_many_numbers = how_many_numbers - 1
   
-  # every happy little bar wants to be painted in a different color becaues 
-  # every bar is different but equaly important. Here the happy little bars want 
+  # every happy little bar wants to be painted in a different color because 
+  # every bar is different but equally important. Here the happy little bars want 
   # to be drawn either green or red.
   happy_little_bars_colors <- scales::seq_gradient_pal("red", "green", "Lab")(seq(0,1,length.out=how_many_numbers))
   
@@ -58,10 +58,10 @@ create_color_pallet <- function(vector_of_possible_values){
   
   # now bob is taking his small detail brush dips it into some color and is 
   # ready to create all those little happy bars. He even thinks about the the 
-  # NA bar that allways gets sad because it only there when somebody didn't want
-  # to share his thougts and rather kept his hopes and dreams by himselve.
-  # Allso if a happy little bar doesn't want to be there, he just paints the 
-  # happy little bars with the color he prommised to paint them
+  # NA bar that always gets sad because it only there when somebody didn't want
+  # to share his thoughts and rather kept his hopes and dreams by himself.
+  # Also if a happy little bar doesn't want to be there, he just paints the 
+  # happy little bars with the color he promised to paint them
   twleve_inch_brush <- scale_fill_manual(values = happy_little_bars_colors , na.value="#75747E" ,drop=F)
   
   return(twleve_inch_brush)
@@ -98,11 +98,11 @@ vector_of_possible_lables = label
 
 
 do_plot <- function(decode_and_validate_df, vector_of_possible_values, vector_of_possible_lables){
-  # bob ross wants to create a beautiful litte picture with lots of 
+  # bob ross wants to create a beautiful little picture with lots of 
   # happy little bars  
   
   # first he needs to know how big his canvas has to be. 
-  # The width of the painting will be determind by the possible happy bars
+  # The width of the painting will be determined by the possible happy bars
   # he can use length(vector_of_possible_values) to create his guides
   # bob also compensates for the NA bar +1.5 
   scale_x <- c(0.5, length(vector_of_possible_values) + 1.5)
@@ -115,7 +115,7 @@ do_plot <- function(decode_and_validate_df, vector_of_possible_values, vector_of
   
   
   
-  # allso bob has to get to know all the bars who want to be on this picture and
+  # also bob has to get to know all the bars who want to be on this picture and
   # how they want to be drawn 
   scale_fill_right = create_color_pallet(vector_of_possible_values)
 
